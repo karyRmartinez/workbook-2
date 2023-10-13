d79847a41468e9cb37f3554fe0f49222de93880e
karyRmartinez <karymartinez@pursuit.org>
2023-10-13 09:20:54 -0400

    fullNameGenerator Exercise

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FullNameApplication {
+
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        Scanner scanner = new Scanner(System.in);
+
+        String message = "Yikes Stripes!";
+        System.out.println(message);
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 0; i < message.length() ; i++) {
+            System.out.println( message.charAt(i) );
+        }
+
+        for (int i = message.length() - 1; i >=0 ; i--) {
+            System.out.println( message.charAt(i) );
+        }
+
+        String first = getStringInput(scanner, "Enter your first name: ").trim();
+        String middle = getStringInput(scanner, "Enter your middle name or initial: ").trim();
+        String last = getStringInput(scanner, "Enter your last name: ").trim();
+        String title = getStringInput(scanner, "Enter your title: ").trim();
+
+        // Print out raw input without any spacing
+        System.out.println(first + middle + last + title);
+
+        first = first.replaceFirst( first.substring(0,1), first.substring(0,1).toUpperCase());
+
+        if ( middle.length() == 1 ) middle = middle + ".";
+
+       // if ( ! title.isBlank() ) title = ", " + title;
+        title = title.toUpperCase();
+
+        System.out.println(first + " " + middle + " " + last + title);
+
+
+
+
+    }
+
+    public static String getStringInput(Scanner scanner, String prompt){
+        System.out.print(prompt);
+        String input = scanner.nextLine();
+        return input;
+    }
+}
